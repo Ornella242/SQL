bb08f8332abbe8504e7667b956a5e61a276b436a
Charles GBOYOU <gboyoucharles22@gmail.com>
2021-12-11 04:02:57 +0100

    BDD, Connexion, Fonction, Traitement

---
+++ vue/commande.php
@@ -0,0 +1,59 @@
+<?php
+$id_cate = 1;
+?>
+<div class="container">
+    <div class="row">
+        <div class="col-sm-4">
+            <div class="list-group " id="list-tab" role="tablist" style="margin-top:90px; margin-left: 5px">
+                <a class="list-group-item list-group-item-action p-3 mb-2 bg-secondary text-white" id="list-home-list" data-bs-toggle="list" href="#list-home" role="tab" aria-controls="list-home">SQL</a>
+                <?php
+                    foreach (get_all_cmd( $id_cate ) as $key => $value) {
+                        ?>
+<a class="list-group-item list-group-item-action" id="list-profile-list" data-bs-toggle="list" href="#<?= $value['rolecmd'] ?>" role="tab" aria-controls="list-profile"><?= $value['rolecmd'] ?></a>
+                        <?php
+                    }
+                ?>
+            </div>
+        </div>
+        <div class="col-sm-8 " style="margin-top:120px;">
+            <h1 style=" text-shadow:1px 1px 1px black; "> <?= text_maj(get_categorie(1)); ?></h1>
+            <?php
+            if(get_lien_img($id_cate) != null){
+                ?>
+                    <img src="../images/<?= get_lien_img( $id_cate ) ?>" class="rounded img-fluid" alt="...">
+                <?php
+            }
+
+            if (get_mean_cmd($id_cate) != '') {
+            ?>
+                <h2 style="text-decoration: underline;" class="pt-4">Que signifie le langage SQL?</h2>
+                <p>
+                    <?= get_mean_cmd(1); ?>
+                </p>
+            <?php
+            }
+            if (get_why_cmd($id_cate) != '') {
+            ?>
+                <h2 style="text-decoration: underline;">Pourquoi coder en SQL ?</h2>
+                <p>
+                    <?= get_why_cmd( $id_cate ); ?>
+                </p>
+            <?php
+            }
+            ?>
+            <h3 style="text-decoration: underline;">Les Commandes SQL</h3>
+            <ul>
+                <ol>
+                <?php
+                        foreach (get_all_cmd($id_cate) as $key => $value) {
+                            ?>
+                            <li class="fst-italic fw-bolder"><?= $value['rolecmd'] ?></li>
+                            <div class="p-3 mb-2 bg-secondary text-white rounded-pill" id="<?= $value['rolecmd'] ?>"><?= $value['commande'] ?></div>
+                            <?php
+                        }
+                    ?>
+                </ol>
+            </ul>
+        </div>
+    </div>
+</div>
---
+++ vue/menu.php
@@ -0,0 +1,35 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+<a class="navbar" href="#" id="title" style="font-weight:bold; text-shadow:1px 1px 1px black; font-size:30px;">CMD</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+  <ul class="navbar-nav mr-auto"></ul>  
+  <ul class="navbar-nav mr-auto"></ul> 
+  <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Accueil</a>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Commandes
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="#">SQL</a>
+          <a class="dropdown-item" href="#">PL/SQL</a>
+          <a class="dropdown-item" href="#">Symfony</a>
+          <a class="dropdown-item" href="#">Git</a>
+          <a class="dropdown-item" href="#">Linux</a>
+        </div>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link disabled" href="#">About</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link disabled" href="#"></a>
+      </li>
+    </ul>
+  </div>
+</nav>
+
