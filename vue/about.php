c04591e15a1c36c467643b52d5fe29ca5e6f60c5
Charles GBOYOU <gboyoucharles22@gmail.com>
2021-12-14 17:34:47 +0100

    suite about

--- vue/about.php
+++ vue/about.php
@@ -1,3 +1,52 @@
-<div class="jumbotron">
+<div class="jumbotron" style="background-color: white;">
+<!--================Home Banner Area =================-->
+<section class="banner_area">
+      <div class="banner_inner d-flex align-items-center">
+        <div class="overlay"></div>
+        <div class="container">
+          <div class="row justify-content-center">
+            <div class="col-lg-6">
+              <div class="banner_content text-center">
+                <h2>À Propos De Nous</h2><br>
+                <h2><i class="ti-arrow-down" aria-hidden="true"></i></h2>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!--================End Home Banner Area =================-->
 
+    <!--================ Start About Area =================-->
+    <section class="about_area">
+      <div class="container">
+        <div class="row h_blog_item">
+          <div class="col-lg-6">
+            <div class="h_blog_img">
+              <img class="img-fluid" src="../images/cmd.jpg" alt="" />
+            </div>
+          </div>
+          <div class="col-lg-6 border border-secondary" style="padding: 50px;">
+            <div class="h_blog_text">
+              <div class="h_blog_text_inner left right">
+                <h4>Bienvenu sur <strong style="color: yellow;">Plus</strong><strong  style="color: blue;">BG</strong></h4>
+                <p>
+                <strong style="color: yellow;">Plus</strong><strong  style="color: blue;">BG</strong> est un site web qui permet de connaitre le plus beau et la plus belle de la saison à travers des votes.
+                  Chaque votant, vote deux fois soit une fois pour une fille et une fois pour un garçon. 
+                </p>
+                <p>
+                   Quelqu'un qui voudrait participer au cours du <strong style="color: yellow;">Plus</strong><strong  style="color: blue;">BG</strong> remplira un formulaire. Si il remplit les conditions de participation, il verra son nom dans la liste des candidats et candidates. <br>
+                   Cliquer sur participer pour être le prochain <strong style="color: yellow;">Plus</strong><strong  style="color: blue;">BG</strong> de la saison.
+                </p>
+                <a class="primary-btn" href="participer.php">
+                  Participer <i class="ti-arrow-right ml-1"></i>
+                </a>
+                <h6>Merci pour le passage<i class="ti-heart" aria-hidden="true"></i></h6>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!--================ End About Area =================-->
 </div>
